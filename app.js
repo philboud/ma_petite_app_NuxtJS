4edef8b8c65166f2de61a6174e41565bf8a31035
Philippe.boudet <philippe.boudet42@gmail.com>
2021-03-12 11:53:45 +0100

    deploy heroku

--- app.js
+++ app.js
@@ -26,10 +26,6 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.use(express.static(path.join(__dirname, "./dist")))
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './dist', 'index.html'))
-})
-
 
 // Fetch all profiles
 app.get('/profiles', (req, res) => {
